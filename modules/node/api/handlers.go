e06103b2d8f43283576c706106f154614ad99f23
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-04-01 12:32:40 +0530

    node: Implement handlers for API endpoints

--- modules/node/api/handlers.go
+++ modules/node/api/handlers.go
@@ -1,1 +1,20 @@
 package api
+
+import "net/http"
+
+func SaveMsgHandler(w http.ResponseWriter, r *http.Request) {
+	//todo
+}
+
+func GetMsgHandler(w http.ResponseWriter, r *http.Request) {
+	//todo
+}
+
+func IsAliveHandler(w http.ResponseWriter, r *http.Request) {
+
+	//todo
+}
+
+func TimeSignHandler(w http.ResponseWriter, r *http.Request) {
+	//todo
+}
