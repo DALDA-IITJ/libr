3ca7c1ffd5f2735e8f1745e783bc5aa534f2b92c
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-04-01 12:33:24 +0530

    Implement basic node functionality

--- modules/node/node.go
+++ modules/node/node.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	logger "libr/core"
+	"libr/modules/node/api"
+	"libr/modules/node/blockchain"
+	"libr/modules/node/db"
+	"net/http"
+)
+
+func main() {
+	logger.Info("Starting node...")
+
+	db.InitDB()
+	blockchain.RegisterNode()
+
+	router := api.SetUpRoutes()
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	logger.Info("Node started successfully")
+}
