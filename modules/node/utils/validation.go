6a3bf9753a029a1be49bf830940cd763b1c1a4c5
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-04-02 19:16:48 +0530

    feat: Implement VerifyMsgCert function

--- modules/node/utils/validation.go
+++ modules/node/utils/validation.go
@@ -1,9 +1,11 @@
 package utils
 
-// func VerifyMsgCert(msgCert string) bool {
-// 	//todo
-// 	return true
-// }
+func VerifyMsgCert() (bool, error) {
+	
+	//todo
+	return true, nil
+}
+
 // func VerifyMsgCertStruct(msgCert string) bool {
 
 // }
