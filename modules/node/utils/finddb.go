04aa2634e11884ff86eb12b94b60ed174db2d05f
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-04-02 19:16:48 +0530

    feat: Add FindKMods function and update FindKServers

--- modules/node/utils/finddb.go
+++ modules/node/utils/finddb.go
@@ -1,6 +1,13 @@
 package utils
 
-//todo
-func FindKServers(msg string) {
+// todo
+func FindKServers(timeStamp int64) ([]string, error) {
+	//todo
+	return nil, nil
 
+}
+
+func FindKMods(timeStamp int64) ([]string, error) {
+	//todo
+	return nil, nil
 }
