e48d156edd4313dc9db6073f4c01d916d8945778
Bot2048 <arpanjain0001@gmail.com>
2025-04-11 02:29:51 +0530

    Config for mod

---
+++ modules/mod/config.go
@@ -0,0 +1,22 @@
+package main
+
+var (
+	ToxicWeight            = 0.9
+	InsultWeight           = 0.7
+	ProfanityWeight        = 0.8
+	DerogatoryWeight       = 0.2
+	SexualWeight           = 0.2
+	DeathHarmTragedyWeight = 0.1
+	ViolentWeight          = 0.3
+	FirearmsWeaponsWeight  = 0.1
+	PublicSafetyWeight     = 0.1
+	HealthWeight           = 0.1
+	ReligionBeliefWeight   = 0.05
+	DrugsWeight            = 0.1
+	WarConflictWeight      = 0.05
+	PoliticsWeight         = 0.05
+	FinanceWeight          = 0.05
+	LegalWeight            = 0.1
+
+	Threshold = 0.6
+)
