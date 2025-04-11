3a531c3758d4a971ba8cba03dc67d21635973cca
Aradhya2708 <aradhyamahajan2708@gmail.com>
2025-04-11 05:32:38 +0530

    seperate command handlers

---
+++ modules/UI/CLI/repl/commands.go
@@ -0,0 +1,74 @@
+package repl
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/DALDA-IITJ/libr/modules/client/core"
+)
+
+func handleSendCommand(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: send <message>")
+		return
+	}
+
+	message := strings.Join(args, " ")
+	core := core.NewCore()
+	err := core.SendMessage(message)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("✔ Message sent successfully")
+	}
+}
+
+func handleFetchCommand(timestamp int64) {
+	core := core.NewCore()
+	messages, err := core.FetchMessages(fmt.Sprint(timestamp))
+	if err != nil {
+		fmt.Println("Error fetching messages:", err)
+		return
+	}
+
+	fmt.Println("\n=== Messages After Timestamp ===")
+	if len(messages) == 0 {
+		fmt.Println("No newer messages found.")
+	} else {
+		for _, msg := range messages {
+			fmt.Printf("[%s] [%s]=> %s\n", time.Unix(timestamp*100, 0).Format("2006-01-02 15:04:05"), msg.Sender, msg.Content)
+		}
+	}
+	fmt.Println("==============================\n")
+}
+
+func handlePrevCommand(currentBucket int64) int64 {
+	prevBucket := currentBucket - 1
+	core := core.NewCore()
+	messages, err := core.FetchMessages(fmt.Sprint(prevBucket))
+	if err != nil {
+		fmt.Println("Error fetching messages:", err)
+		return currentBucket
+	}
+
+	fmt.Println("\n=== Messages in Previous Bucket ===")
+	if len(messages) == 0 {
+		fmt.Println("No messages found in previous bucket.")
+	} else {
+		for _, msg := range messages {
+			fmt.Printf("[%s] [%s] => %s\n", time.Unix(currentBucket*100, 0).Format("2006-01-02 15:04:05"), msg.Sender, msg.Content)
+		}
+	}
+	fmt.Println("===============================\n")
+	return prevBucket
+}
+
+func printHelp() {
+	fmt.Println("\n=== CLI Commands ===")
+	fmt.Println("send <message>     - Send a message.")
+	fmt.Println("f, fetch           - Fetch messages after the current timestamp.")
+	fmt.Println("p, prev            - Fetch messages before the current timestamp.")
+	fmt.Println("\\q                 - Quit the CLI.")
+	fmt.Println("====================\n")
+}
